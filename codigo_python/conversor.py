46ad515141918319e598fe724f7acc79cf0fa07c
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-09 02:36:44 -0500

    Codigo finalizado para tres opciones de monedas

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -10,21 +10,29 @@ Elige una opción:
 opcion = input(menu)
 
 if opcion == '1':
-    pass
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $"+ dolares + " dólares" )
+elif opcion == '2':
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $"+ dolares + " dólares" )
+elif opcion == '3':
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 20
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $"+ dolares + " dólares" )
+else:
+    print('Ingresa una opción correcta por favor')
 
-pesos = input("¿Cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $"+ dolares + " dólares" )
-
-#Reto inverso
-dolar = input("\n¿Cuántos dólares tienes?: ")
-dolar = float(dolar)
-valor_pesos_mexicano = 0.050
-peso = dolar / valor_pesos_mexicano
-peso = round(peso, 2)
-peso = str(peso)
-print("Tienes $"+ peso + "  pesos mexicanos" )
