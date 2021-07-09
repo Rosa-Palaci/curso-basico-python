f90f34f998759437017119deb53c3ee5ac41f01e
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-09 02:11:41 -0500

    Reto cumplido video 13

--- codigo_python/conversor.py
+++ codigo_python/conversor.py
@@ -1,9 +1,17 @@
-#13 - Tu primer programa: conversor de monedas
-
-pesos = input("¿Cuántos pesos colombianos tiene?: ")
+#Reto: conversor de monedas
+pesos = input("¿Cuántos pesos mexicanos tienes?: ")
 pesos = float(pesos)
-valor_dolar = 3875
+valor_dolar = 20
 dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
-print("Tienes $"+ dolares + "dólares" )
+print("Tienes $"+ dolares + " dólares" )
+
+#Reto inverso
+dolar = input("\n¿Cuántos dólares tienes?: ")
+dolar = float(dolar)
+valor_pesos_mexicano = 0.050
+peso = dolar / valor_pesos_mexicano
+peso = round(peso, 2)
+peso = str(peso)
+print("Tienes $"+ peso + "  pesos mexicanos" )
