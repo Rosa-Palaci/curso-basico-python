6aaa9183fe91207ab7f8544fb8a96029804eb0ba
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-10 13:00:46 -0500

    ciclo for

---
+++ codigo_python/for.py
@@ -0,0 +1,21 @@
+#Imprimir de los numeros del 1 al 1000
+'''
+print(1)
+print(2)
+print(3)
+print(4)
+
+contador = 1
+print(contador)
+while contador < 1000:
+    contador += 1
+    print(contador)
+
+#resolver con ciclo for
+
+for contandor in range(1000):
+    print(contandor)'''
+
+# tabla del 11
+for i in range(10):
+    print(11*i)
