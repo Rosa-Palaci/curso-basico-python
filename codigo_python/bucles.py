947908eaae0c2b14876c66697f6424ac24c77ab1
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-10 00:03:49 -0500

    Aprendiendo bucles

--- codigo_python/bucles.py
+++ codigo_python/bucles.py
@@ -1,8 +1,11 @@
+'''
 #Imprimir todas la potencial de 2 hasta llegar al numero 1000
 
 for num_potencia in range(1, 1001):
     po = num_potencia**2
     print(po)
 
-
 # print(num_potencia)
+'''
+contador = 1
+print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
