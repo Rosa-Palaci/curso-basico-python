63e630d1446d839ee2015c5091d38636d0b52d01
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-10 16:02:23 -0500

    Interrumpiendo ciclos con break y continue

---
+++ codigo_python/break.py
@@ -0,0 +1,22 @@
+def run():
+    '''
+    for contador in range(1000):
+        if contador % 2 != 0:#impirmir solo numeros pares-->
+            continue
+        print(contador)
+    
+    for i in range(10000):
+        print(i)
+        if i == 5678:
+            break
+    
+    text = input('Escribe un texto: ')
+    for letra in text:
+        if letra == 'o':
+            break
+        print(letra)'''
+#Reto donde utilice break y continue, ciclo while
+
+
+if __name__ == '__main__':
+    run()
