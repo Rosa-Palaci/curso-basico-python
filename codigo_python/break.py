b7cc430c1f0383c2e371aab6d99de1495f9907a8
Rosa Palaci <palacibelrose431@gmail.com>
2021-07-10 16:32:00 -0500

    Reto utilizando ciclo while y break, continues

--- codigo_python/break.py
+++ codigo_python/break.py
@@ -16,6 +16,31 @@ def run():
             break
         print(letra)'''
 #Reto donde utilice break y continue, ciclo while
+def run():
+    print('Este es mi reto utilizando ciclo while y break y continue')
+    print('Completa el dicho')
+
+    dichos = """
+    Bienvenido al Cafe del Dicho 🤷‍♀️😉
+    1 - Agua que no has de beber, ... 
+    2 - Botellita de jerez, ... 
+    3 - El flojo y el mezquino, ... 
+    4 - No quiero jugar solo deme mi café 🤔
+
+    Elige una mitad del dicho que quieras acompletar: 
+    """ 
+    dicho_elegido = input(dichos)
+    respuesta_dada = input('Ingresa la otra parte del dicho: ')
+
+    RESPUESTA = """
+    1 = 'déjala correr'
+    2 = 'todo lo que me digas será al revés'
+    3 = 'recorren dos veces el mismo camino'
+    """
+    while respuesta_dada != (RESPUESTA + '{dicho_elegido}'): 
+        print('Auch!' + str(respuesta_dada) + ' no es la otra mitad, elige denuevo ')
+        break
+        
 
 
 if __name__ == '__main__':
